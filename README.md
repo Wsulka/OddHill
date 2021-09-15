8365b3baa4ba72058b99a2f78bafea6c48f6396d
William Sulka <williamsulka@gmail.com>
2021-09-15 10:29:48 +0200

    3rd commit laptop
    	#	modified:   style.css

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Odd hill front end worksample
 
 The design says that desktop should be 1290 px wide and 12 columns, however the wireframe is 1440 px and apparently full width.
+
---
+++ books.js
@@ -0,0 +1,76 @@
+let books = [
+
+    {
+        "title":"Steve Jobs",
+        "author":"Walter Isaac",
+        "image":"book1.png",
+        "score":3.5,
+    },
+    {
+        "title":"Throne of Glass",
+        "author":"Sarah J Maas",
+        "image":"book2.png",
+        "score":3,
+    },
+    {
+        "title":"Unspeakable Things",
+        "author":"Jess Lourey",
+        "image":"book3.png",
+        "score":"4",
+    },
+    {
+        "title":,
+        "author":,
+        "image":,
+        "score":,
+    },
+    {
+        "title":,
+        "author":,
+        "image":,
+        "score":,
+    },
+    {
+        "title":,
+        "author":,
+        "image":,
+        "score":,
+    },
+    {
+        "title":,
+        "author":,
+        "image":,
+        "score":,
+    },
+    {
+        "title":,
+        "author":,
+        "image":,
+        "score":,
+    },
+    {
+        "title":,
+        "author":,
+        "image":,
+        "score":,
+    },
+    {
+        "title":,
+        "author":,
+        "image":,
+        "score":,
+    },
+    {
+        "title":,
+        "author":,
+        "image":,
+        "score":,
+    },
+    {
+        "title":,
+        "author":,
+        "image":,
+        "score":,
+    },
+   
+]
